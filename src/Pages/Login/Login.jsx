96ca3a719ab0e6ac1a62ee8e944948133f220947
Norhanahmedsaber <norhan3182001@gmail.com>
2023-12-10 16:50:50 +0200

    ..

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,24 +2,44 @@ import React, { useState } from "react";
 import Button from "../../Components/Button";
 import LoginForm from "./LoginForm";
 import Footer from "../../Components/Footer";
+import Logo from "../../Components/Logo"
+import config from "../../../config";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
     const [email,setEmail] = useState("")
     const [password,setPassword]=useState("")
-    function Fetch() {
-        console.log(email,password)
+    const [error, setError] = useState("")
+    const nav = useNavigate()
+    function handleLogin() {
+        fetch(config.BASE_URL+'/login',{
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body:JSON.stringify({
+                email,
+                password
+            })
+        }).then(response => response.json())
+        .then((result)=>{
+            if(result.message){
+                setError(result.message)
+            }else {
+                nav('/')
+            }
+        })
     }
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <div className="w-[12%] ">
-                <img className="w-full h-full" src="../../../public/logo.png " />
-            </div>
+            <Logo path={"../../../public/logo.png"}/>
+            <div>{error}</div>
             <LoginForm
                 email={email}
                 setEmail={setEmail} 
                 password={password} 
                 setPassword={setPassword} 
-                setDone={Fetch}
-                />
+                setDone={handleLogin}
+            />
             <Footer
             text={"Doesnot have an account ?"}
             path={'/signup'}
