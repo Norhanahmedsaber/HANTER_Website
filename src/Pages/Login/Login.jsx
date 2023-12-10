c482fb398af045ea3c912b0c3d3a3aee4c001ee1
anas12312 <anashesham000@gmail.com>
2023-12-10 14:17:02 +0200

    example

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Anas from "./anas";
+import Button from "../../Components/Button";
 export default function Login() {
+    const [counter, setCounter] = useState(0)
+    function anasFetch() {
+        console.log(counter)
+    }
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <Anas />
-            <Anas />
-            <Anas />
+            <Button 
+                counter={counter} 
+                setCounter={setCounter} 
+            />
+            <div onClick={anasFetch} className="hover:cursor-pointer">
+                Send
+            </div>
         </div>
     )
 }
