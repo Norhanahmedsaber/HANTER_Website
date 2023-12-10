7628a2c62f1abe8995acd1cbdedef19c3bf5c920
Norhanahmedsaber <norhan3182001@gmail.com>
2023-12-10 16:18:30 +0200

    login

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 import Button from "../../Components/Button";
 import LoginForm from "./LoginForm";
+import Footer from "../../Components/Footer";
 export default function Login() {
     const [email,setEmail] = useState("")
     const [password,setPassword]=useState("")
-    function anasFetch() {
+    function Fetch() {
         console.log(email,password)
     }
     return (
         <div className="flex flex-col justify-center items-center h-screen">
+            <div className="w-[12%] ">
+                <img className="w-full h-full" src="../../../public/logo.png " />
+            </div>
             <LoginForm
                 email={email}
                 setEmail={setEmail} 
                 password={password} 
                 setPassword={setPassword} 
-                setDone={anasFetch}
+                setDone={Fetch}
                 />
+            <Footer
+            text={"Doesnot have an account ?"}
+            path={'/signup'}
+            pressableText={"Sign Up"}
+            />    
         </div>
+
     )
 }
