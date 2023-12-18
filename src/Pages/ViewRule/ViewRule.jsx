504661e1fc2e549ebbd4d954000b3728c5e9cbfc
anas12312 <anashesham000@gmail.com>
2023-12-18 12:28:26 +0200

    rule viewer

--- src/Pages/ViewRule/ViewRule.jsx
+++ src/Pages/ViewRule/ViewRule.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import YamlViewer from './YamlViewer'
+import './styles.css'
 export default function ViewRule() {
+
   return (
-    <div>ViewRule</div>
+    <div className='w-full h-full flex justify-center items-center'>
+      <div className='btn-view-rule bg-red-400'>a</div>
+      <div className='btn-view-rule bg-blue-400'>b</div>
+      <YamlViewer />
+    </div>
   )
 }
