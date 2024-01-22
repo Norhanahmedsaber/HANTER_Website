770909bd30e52e64b551f4f4f620afde1e4fd0e8
Norhanahmedsaber <norhan3182001@gmail.com>
2024-01-22 12:55:19 +0200

    anas

---
+++ src/Pages/Playground/CodeViewer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import {javascript} from "@codemirror/lang-javascript"
+export default function CodeViewer({content,setContent}) {
+  return (
+    <div className='w-full h-full'>
+         
+    </div>
+  );
+}
+
+
