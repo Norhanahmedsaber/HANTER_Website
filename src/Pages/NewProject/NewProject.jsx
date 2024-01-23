1106bb28066d2edcdd178a3289bd1f560fb70423
Norhanahmedsaber <norhan3182001@gmail.com>
2024-01-23 17:51:55 +0200

    ..

--- src/Pages/NewProject/NewProject.jsx
+++ src/Pages/NewProject/NewProject.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Sidebar } from '../../Components/Sidebar/Sidebar'
 export default function NewProject() {
   return (
-    <div className='flex justify-start w-screen h-screen items-start border border-red-500'>
-        <Sidebar/>
+    <div className='flex justify-start w-screen h-screen items-start border'>
+        <Sidebar selected={"docs"} />
 
       </div>
   )
