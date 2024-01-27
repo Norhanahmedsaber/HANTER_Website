5b9c5c901d6cc23656dec18ce837d1559b12ed4d
2152001 <tokaabdelgawad60@gmail.com>
2024-01-27 23:02:47 +0200

    reports

--- src/Pages/Reports/Reports.jsx
+++ src/Pages/Reports/Reports.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import ProjectBar from "./ProjectBar";
+import MatchingSide from "./MatchingSide";
 
 export default function Reports() {
   return (
-    <div>Reports</div>
-  )
+    <div className="flex justify-start w-screen h-screen items-start">
+      <Sidebar />
+      <div className=" w-[calc(100%-12.5rem)] h-full flex flex-col">
+        <div className="border border-b-[#8F8C8C] h-[3.65rem] flex justify-between  items-center">
+          <div className="ml-[1.25rem] text-[2.3rem] font-sem2">Reports</div>
+          <div className="relative">
+            <div className="w-[9.375rem] h-[2.5rem] bg-secondary rounded-[0.625rem] mr-[1.25rem] flex justify-center items-center cursor-pointer">
+              <div className="text-[1.4rem] text-[#FFF] ml-[2rem] font-sem2 ">
+                Re-Scan
+              </div>
+              <div
+                className="absolute left-3
+                          flex items-center  
+                          pointer-events-none"
+              >
+                <img
+                  src={"../../../public/scan-code.png"}
+                  className="w-[1.7rem] h-[1.7rem] "
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex w-full h-full bg-[#EAEAEA]">
+          <div className="border border-r-[#8F8C8C]">
+            <ProjectBar />
+          </div>
+          <div className="w-[calc(100%-18.75rem)]  flex flex-col">
+            <div className="h-[3.125rem] flex justify-start items-center ">
+              <div className="ml-[2.25rem] text-[1.25rem] text-[#000] font-sem2 font-semibold">
+                70 matching found
+              </div>
+            </div>
+            <MatchingSide />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
