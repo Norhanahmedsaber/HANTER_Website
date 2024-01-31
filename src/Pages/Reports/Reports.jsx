a22b84dfa0fb37a3bcaa96fc5df033e226a6e6aa
Norhanahmedsaber <norhan3182001@gmail.com>
2024-02-01 01:09:58 +0200

    ..

--- src/Pages/Reports/Reports.jsx
+++ src/Pages/Reports/Reports.jsx
@@ -12,7 +12,6 @@ export default function Reports() {
   const [error, setError] = useState("")
   const [reports, setReports] = useState([])
   const [projectRules, setProjectRules] = useState([])
-
   function clacMatches(reports) {
     let matchedNum = 0
     reports.map((report) => matchedNum += report.reports.length)
@@ -35,8 +34,22 @@ export default function Reports() {
         }
       })
   }
-
-
+  async function rescan(){
+    const response= await fetch(config.BASE_URL+'/rescan/'+id,{
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: Cookies.get('token')
+      },
+    })
+      const result=await response.json()
+      if(result.message){
+      setError(result.message)
+    }else{
+      loadReports()
+      getProjectRules()
+    }
+  }
   function loadReports() {
     fetch(config.BASE_URL + '/reports/' + id, {
       method: "GET",
@@ -65,7 +78,7 @@ export default function Reports() {
           <div className="ml-[1.25rem] text-[2.3rem] font-sem2">Reports</div>
           <div className="relative">
             <div className="w-[9.375rem] h-[2.5rem] bg-secondary rounded-[0.625rem] mr-[1.25rem] flex justify-center items-center cursor-pointer">
-              <div className="text-[1.4rem] text-[#FFF] ml-[2rem] font-sem2 ">
+              <div className="text-[1.4rem] text-[#FFF] ml-[2rem] font-sem2 " onClick={()=>{rescan()}}>
                 Re-Scan
               </div>
               <div
