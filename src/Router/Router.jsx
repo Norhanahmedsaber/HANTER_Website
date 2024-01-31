5dd61097001c245a63a82df56a1433ce915cdfb9
2152001 <tokaabdelgawad60@gmail.com>
2024-01-31 23:28:55 +0200

    toka

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -50,7 +50,7 @@ export default function Router() {
       element: <NewProject />,
     },
     {
-      path: "/reports/:id",
+      path: "/reports/:name/:id",
       element: <Reports />,
     },
     {
