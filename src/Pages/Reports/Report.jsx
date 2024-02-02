de62e60eeacee7b0e4f0a60cfae070bf151fdb09
anas12312 <anashesham000@gmail.com>
2024-02-03 00:24:31 +0200

    anas

--- src/Pages/Reports/Report.jsx
+++ src/Pages/Reports/Report.jsx
@@ -4,7 +4,7 @@ export default function Report({filePath ,line , col, goToRepo}) {
   return (
     <div onClick={()=>{
       goToRepo(filePath.split('\\').slice(1).join('\\').replace('\\','/').replace(/\\/g, '/'), line)
-    }} className="w-full h-[1.9625rem] pl-4 border border-b-[#8F8C8C] flex justify-between hover:bg-gray-200 cursor-pointer hover:underline">
+    }} className="w-full h-[1.9625rem] pl-4 border border-b-[#8F8C8C] text-blue-700 flex justify-between hover:bg-gray-200 cursor-pointer hover:underline bg-white">
       <div>{
         filePath.split('\\').slice(1).join('\\').replace('\\','/').replace(/\\/g, '/')
       }</div>
