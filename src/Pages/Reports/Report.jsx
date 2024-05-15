2c6adeefd9e595a45c5c1d98640b0c3254e18bef
Norhanahmedsaber <norhan3182001@gmail.com>
2024-05-15 17:31:53 +0300

    anas

--- src/Pages/Reports/Report.jsx
+++ src/Pages/Reports/Report.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Report({filePath ,line , col, goToRepo}) {
+  useEffect(()=>{
+    console.log(filePath, line, col, filePath.split('/').slice(1).join('\\').replace('\\','/').replace(/\\/g, '/'));
+  }, [])
   return (
     <div onClick={()=>{
-      goToRepo(filePath.split('\\').slice(1).join('\\').replace('\\','/').replace(/\\/g, '/'), line)
+      goToRepo(filePath.split('/').slice(1).join('\\').replace('\\','/').replace(/\\/g, '/'), line)
     }} className="w-full h-[1.9625rem] pl-4 border border-b-[#8F8C8C] text-blue-700 flex justify-between hover:bg-gray-200 cursor-pointer hover:underline bg-white">
       <div>{
-        filePath.split('\\').slice(1).join('\\').replace('\\','/').replace(/\\/g, '/')
+        filePath.split('/').slice(1).join('\\').replace('\\','/').replace(/\\/g, '/')
       }</div>
       <div className='mr-10'>{line}</div>
     </div>
